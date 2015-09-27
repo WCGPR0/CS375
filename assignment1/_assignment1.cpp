b6734662796580cc11cff67bf485890e4703c903
Victor Wu <vwu4@cs.binghamton.edu>
2015-09-26 21:38:00 -0400

    First Commit

---
+++ assignment1/_assignment1.cpp
@@ -0,0 +1,35 @@
+/* Victor Wu
+   CS375
+   09/23/2015
+*/
+
+/// This is an assignment program in which the task is to determine whether there exists such a key which is equal to the summation of other two keys in the array, given an array.
+
+
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char* argv[]) {
+
+
+
+return 0;
+}
+
+
+//Implemented via psuedo code from Wikipedia: Insertion Sort
+void custom_insertion_sort(int array[], int length) {
+	int a, b, temp;
+	for (int a = 1; a < length; ++a) {
+	b = a;
+	while (b > 0 && array[b - 1] > array[b]) {
+
+
+	}
+
+
+	}
+
+}
---
+++ assignment1/assignment1.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cstring>
+
+using namespace std;
+
+#define initialSize 50
+
+//Public GCC compiler-friendly macros
+#define min(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a < _b ? _a : _b; })
+
+#define max(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a > _b ? _a : _b; })
+
+int binary_search(int *A, int firstIndex, int lastIndex, int sum);
+
+int main (int argc, char* argv[]) {
+if (argc != 3) { cerr << "Error, invalid amount of arguments. Refer to README for details on usage, or use command --help" << endl; return -1;}
+if (strcmp(argv[1], "--help") == 0) { cout << "Usage: ./victor_wu_C++ [input] [output]" << endl; return 0; }
+
+int n = 0, size = 0;
+int *A = new int[initialSize];
+int *B = new int[initialSize];
+
+fstream myFile(argv[1], ios_base::in);
+ofstream myFileOutput(argv[2]);
+
+int sum = 0;
+
+while (myFile >> A[size++]) {
+   //Code based on Psuedo-code Wikipedia: Insertion Sort
+   for (int i = 0; i < size; i++) {
+	int j = i;
+	while (j > 0 && A[j] < A[j-1]) {
+		A[j] ^= A[j-1];
+		A[j-1] ^= A[j];;
+		A[j] ^= A[j-1];	
+		j--;
+	}
+		
+   }
+sum+=A[size-1];
+}
+size--;
+
+for (int i = 0; i < size; i++) B[i] = sum - A[i];
+
+// Binary search for complement
+for (int i = 0; i < size; i++) {
+	for (int j = 0; j < size; j++) {
+		if (binary_search(&*A, min(A[i],A[j]), size-1, A[i]-A[j]) != -1) myFileOutput << max(A[i],A[j]) << endl;	
+	}
+}
+
+
+delete [] A;
+
+
+myFileOutput.close();
+myFile.close();
+return 0;
+}
+
+	//Code based on Psuedo-code Wikipedia: Binary Sort
+	int binary_search(int *A, int firstIndex, int lastIndex, int sum) {
+	int returnIndex;
+	if (firstIndex > lastIndex) returnIndex = -1;
+	else {
+	int midIndex = (firstIndex + lastIndex) / 2;
+	if (sum == A[midIndex]) returnIndex = midIndex;
+	else if (sum < A[midIndex]) returnIndex = binary_search(A, firstIndex, midIndex-1, sum);
+	else returnIndex = binary_search(A, midIndex+1, lastIndex, sum);
+	}
+	return returnIndex;
+}
+
+/*for (int i = size; i > 0; i--) {
+int a = 0,b = 1;
+
+while (a < size && A[a]+A[size-b] < A[i]) {a++;}
+--a;
+if (A[a]+A[size-b] == A[i] ) printf("%d", A[i]);
+else if (A[a]+A[size-b] < A[i]) break;
+else {
+a = 0;
+b++;
+}
+
+else if (A[i]+A[i+1] > A[size - i] )
+else
+	printf("%d", A[i]); 
+
+}
+
+
+sum += A[size-1];
+}
+
+*/
+
+
+// TO DO ALGORITHM HERE
+/*printf("Arrays: ");
+for (int i = 0; i < size; i++)
+   printf("\t%d", A[i]);
+printf("\n");
+*/
+
+void resize(int *A, int size, size_t oldSize) {
+   oldSize *= 2;
+   int *newArray = new int[oldSize];
+   for (int i = 0; i < size; i++)
+      newArray[i] = A[i];
+   delete [] A;
+   A = newArray;
+}
+
