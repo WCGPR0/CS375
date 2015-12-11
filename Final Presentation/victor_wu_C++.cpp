a072b2c4936bf1d050e11c7fae2909c848c82e33
Victor Wu <vwu4@cs.binghamton.edu>
2015-12-11 02:03:40 -0500

    Preparing to  change hashmap to dyanmic array. wut

--- Final Presentation/victor_wu_C++.cpp
+++ Final Presentation/victor_wu_C++.cpp
@@ -9,6 +9,9 @@
 #include <vector>
 #include <queue>
 #include <list>
+#include <unordered_map>
+
+#define DEBUG 1
 
 struct edge {
 	int v1, v2, value;
@@ -39,53 +42,42 @@ void addEdge(std::vector<edge> &myTree, int v1, int v2, int value) {
 //! \param The starting source
 //! \param The ending destination
 //! \return the path from starting to end
-bool bfs(std::vector<edge> myGraph, int source, int sink) {
-	std::list<bool> visited(myGraph.size());
-	std::vector<edge> path(myGraph.size());
+bool bfs(std::vector<edge> myGraph, int source, int sink, std::vector<edge> &path) {
+	std::unordered_map<int, bool> visited;	
+	visited[source] = true;
 	std::queue<int> q;
 	q.push(source);
-	visited[source] = true;
-	
 	while (!q.empty()) {
 		int u = q.front();
-		q.pop();
+			q.pop();
+
 		for (std::vector<edge>::iterator it = myGraph.begin(); it != myGraph.end(); ++it) {
 			int i = std::distance(myGraph.begin(), it);
-			if (visited[i] == false) {
-				q.push(i);
-				path[i] = it->value;
+//if (DEBUG) { for(auto it = visited.begin(); it != visited.end(); ++ it) std::cout << it->first << "," << it->second << "\t";std::cout << std::endl;}
+if (DEBUG) { std::cout << "u: " << u << "\t" << "it->v1: " << it->v1 << std::endl; }	
+			if ((u == it->v1) && (visited[i] != true)) {	
+				path.push_back( *it );
 				visited[i] = true;
+				q.push(it->v2);
+				it = myGraph.begin();
 			}
 
 		}
-
-	}	
-	return (visited[sink] == true);	
-} 
-
-//! \param The tree
-//! \param The starting source
-//! \param The ending destination
-//! \return the maximum possible flow
-int letUsFordFulkThisShit(std::vector<edge> myGraph, int source, int sink) {
-	std::vector<edge> resGraph = myGraph;
-	std::vector<edge> path(myGraph.size());
-	int max_flow = 0;
-	while (bfs(resGraph, source, sink)) {
-		int path_flow = (unsigned int) ~0 >> 1;
-		for (std::vector<edge>::iterator rt = resGraph.begin(); rt != resGraph.end(); ++rt)
-			path_flow = path_flow <= rt->value ? path_flow : rt->value;
-		for (std::vector<edge>::iterator rt = resGraph.begin(); rt != resGraph.end(); ++rt) {
-			rt -= path_flow;
-			rt += path_flow;
-		}
-
-		max_flow += path_flow;
-
 	}
-		return max_flow;
-
+if (DEBUG) std::cout << visited[sink] << std::endl;
+	return (visited[sink] == true);	
 }
+
+
+//! Returns the max vertices
+/*maxNumberOfVertices(std::vector<edge> myEdges) {
+	int max = 0;
+	for (std::vector<edge>::iterator it = myEdges.begin(); it != myEdges.end(); ++it) {
+		if (it->v1 > max) max = it->v1;
+		if (it->v2 > max) max = it->v2;
+	}
+} */
+
 
 int main (int argc, char* argv[]) {
 	if (argc >= 2) { if (strcmp(argv[1],"--help") == 0) { std::cout << "Usage: ./victor_wu_C++ [input] [output]" << std::endl << "\tinput" << std::endl; return 0; }
@@ -102,7 +94,31 @@ int main (int argc, char* argv[]) {
 				addEdge(myTree, v1, v2, value);
 			}
 
-			std::cout << "The max possible flow is: " << letUsFordFulkThisShit(myTree, 0, 5) << std::endl;
+//if (DEBUG) for (std::vector<edge>::iterator it = myTree.begin(); it != myTree.end(); ++it) std::cout << it->v1 << std::endl;	
+
+
+			//Ford Fulkerson Algorithm
+			std::vector<edge> resGraph = myTree;	
+			int max_flow = 0;
+			std::vector<edge> path(resGraph.size());
+
+			int source = 0;
+			int sink = 5;
+
+			while (bfs(resGraph, source, sink, path)) {
+				int path_flow = (unsigned int) ~0 >> 1;
+				for (std::vector<edge>::iterator rt = resGraph.begin(); rt != resGraph.end(); ++rt)
+					path_flow = path_flow <= rt->value ? path_flow : rt->value;
+				for (std::vector<edge>::iterator rt = resGraph.begin(); rt != resGraph.end(); ++rt) {
+					rt -= path_flow;
+					rt += path_flow;
+				}
+
+				max_flow += path_flow;
+
+			}	
+
+			std::cout << "The max possible flow is: " << max_flow << std::endl;
 
 
 			myFileOutput.close();
